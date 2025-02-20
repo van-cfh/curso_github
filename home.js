b04ee106c86307b1766038163d70d2e96b22eb57
devvanessa <van.cfh@gmail.com>
2025-02-20 18:48:51 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome){
     return nome
 }
+
+function mostraNSobrenome(sobrenome){
+    return sobrenome;
+}
+
+//Finalizado sistema da home
--- login.js
+++ login.js
@@ -2,3 +2,12 @@
 
 
 //Finalizando sistema de login
+
+function login(nome, email){
+    return 'Bem vindo'
+}
+
+//Seja bem vindo
+
+//Sistema de login completo
+//Retornamos o nome de usuario quando logado
